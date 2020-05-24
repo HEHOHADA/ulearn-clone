f1832ad609291a08a472f74c92c604775f7bf5be
AlexandrJunushaliev(Hikirangi) <dzhunall@mail.ru>
2020-05-24 20:56:03 +0500

    [start]

--- Client/src/modules/pages/Admin/Subscription/SubscriptionCreate.tsx
+++ Client/src/modules/pages/Admin/Subscription/SubscriptionCreate.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {SubscriptionForm} from "../../../components/pay/SubscriptionForm";
+import {useHttp} from "../../../hooks/http.hook";
+import {ISubscription} from "../../../shared/interface";
+import {subscriptionRequest} from "../../../shared/request";
 
 export const SubscriptionCreate = () => {
-
-    const submit = (event: any, form: any) => {
+    const {request} = useHttp()
+    const submit = async (event: any, form: ISubscription) => {
         event.preventDefault()
-        console.log(form)
+        await request(subscriptionRequest, 'POST', {...form})
     }
 
     return (
--- Client/src/modules/shared/request.ts
+++ Client/src/modules/shared/request.ts
@@ -6,6 +6,8 @@ export const groupRequest = URL + "api/Group"
 export const courseRequest = URL + "api/Course"
 export const moduleRequest = URL + "api/Module"
 export const subscriptionRequest = URL + "api/Subscription"
-export const testTackRequest = URL +"api/TestTask"
+export const testTackRequest = URL + "api/TestTask"
+export const videoTaskRequest = URL+"api/VideoTask"
+export const codeTaskRequest = URL+"api/CodeTask"
 
 
