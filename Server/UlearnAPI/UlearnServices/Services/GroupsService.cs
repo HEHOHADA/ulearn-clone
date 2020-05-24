e7ff3e160a3b1d42c0df654bb24002e01530663c
Ravil <graf.rav@gmail.com>
2020-05-25 00:50:14 +0300

    groupWIP

--- Server/UlearnAPI/UlearnServices/Services/GroupsService.cs
+++ Server/UlearnAPI/UlearnServices/Services/GroupsService.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UlearnData;
 using UlearnData.Models;
+using UlearnServices.Models.Group;
 
 namespace UlearnServices.Services
 {
     public class GroupsService
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public GroupsService(ApplicationDbContext context)
+        public GroupsService(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public bool GroupExists(int id)
@@ -25,7 +29,7 @@ namespace UlearnServices.Services
         {
             return await _context.Groups
                 .Include(group => group.UserGroups)
-                    .ThenInclude(userGroup => userGroup.User)
+                .ThenInclude(userGroup => userGroup.User)
                 .ToListAsync();
         }
 
@@ -33,19 +37,46 @@ namespace UlearnServices.Services
         {
             return await _context.Groups
                 .Include(group => group.UserGroups)
-                    .ThenInclude(userGroup => userGroup.User)
+                .ThenInclude(userGroup => userGroup.User)
                 .FirstOrDefaultAsync(module => module.Id == id);
         }
 
-        public async Task<Group> CreateAsync(Group group)
+        public async Task<Group> CreateAsync(GroupDto model)
         {
+            var group = new Group
+            {
+                Name = model.Name,
+                Course = await _context.Courses.FindAsync(model.CourseId),
+            };
+
+            group.UserGroups = (await Task.WhenAll(model.Emails
+                    .Select(x => _userManager.FindByEmailAsync(x))))
+                .Select(x => new UserGroup
+                {
+                    Group = group,
+                    User = x
+                })
+                .ToList();
+
             _context.Groups.Add(group);
             await _context.SaveChangesAsync();
             return group;
         }
 
-        public async Task PutAsync(Group group)
+        public async Task PutAsync(int id, GroupDto model)
         {
+            var group = await _context.Groups.FindAsync(id);
+
+            group.Course = await _context.Courses.FindAsync(model.CourseId);
+            group.UserGroups = (await Task.WhenAll(model.Emails
+                    .Select(x => _userManager.FindByEmailAsync(x))))
+                .Select(x => new UserGroup
+                {
+                    Group = group,
+                    User = x
+                })
+                .ToList();
+
             _context.Entry(group).State = EntityState.Modified;
             await _context.SaveChangesAsync();
         }
