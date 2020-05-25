e470daf2cc4cb3cd28ef0bfbf68b71243b3560cf
Ravil <graf.rav@gmail.com>
2020-05-25 20:34:58 +0300

    remove module bycourse

--- Server/UlearnAPI/UlearnServices/Services/ModulesService.cs
+++ Server/UlearnAPI/UlearnServices/Services/ModulesService.cs
@@ -67,13 +67,5 @@ namespace UlearnServices.Services
             _context.Modules.Remove(module);
             await _context.SaveChangesAsync();
         }
-
-        public async Task<List<Module>> GetByCourse(int courseId)
-        {
-            return await _context.Modules
-                .Include(x => x.Course)
-                .Where(x => x.Course.Id == courseId)
-                .ToListAsync();
-        }
     }
 }
