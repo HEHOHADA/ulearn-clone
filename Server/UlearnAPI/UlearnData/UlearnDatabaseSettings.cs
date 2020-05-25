dee73e4cd8d4ada6b85ef9262c3e40b1e1766d33
Royal Jackal <dimaorol@gmail.com>
2020-05-25 15:31:23 +0300

    added mongo logging

---
+++ Server/UlearnAPI/UlearnData/UlearnDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace UlearnData
+{
+    public class UlearnDatabaseSettings : IUlearnDatabaseSettings
+    { 
+        public string LogsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IUlearnDatabaseSettings
+    {
+        string LogsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
