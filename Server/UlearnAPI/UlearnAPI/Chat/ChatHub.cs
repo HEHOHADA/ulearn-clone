b11148fb23e91683c685725667dd588a81d69078
Ravil <graf.rav@gmail.com>
2020-05-27 01:28:37 +0300

    chat hub

--- Server/UlearnAPI/UlearnAPI/Chat/ChatHub.cs
+++ Server/UlearnAPI/UlearnAPI/Chat/ChatHub.cs
@@ -1,14 +1,44 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 
 namespace UlearnAPI.Chat
 {
     public class ChatHub : Hub
     {
+        private static readonly List<(string username, string message)>
+            Messages = new List<(string username, string message)>();
+
+        private const int MessagesListSize = 10;
+
+        public static void AddMessage(string username, string message)
+        {
+            Messages.Add((username, message));
+            if (Messages.Count > MessagesListSize)
+            {
+                Messages.RemoveAt(0);
+            }
+        }
+
+
+        public async Task GetAll()
+        {
+            Console.WriteLine("GetAll was called in ChatHub");
+            List<Task> tasks = new List<Task>();
+            foreach (var message in Messages)
+            {
+                tasks.Add(Clients.Caller.SendAsync(
+                    "messageReceived", message.username, message.message));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
         public async Task NewMessage(string username, string message)
         {
-            Console.WriteLine(username + message);
+            Console.WriteLine($"Message {message} from user with name {username} received in ChatHub");
+            AddMessage(username, message);
             await Clients.All.SendAsync("messageReceived", username, message);
         }
     }
