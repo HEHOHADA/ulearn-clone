d155a6717f1fe947717d37e82d30b00ffaa051dc
Ravil <graf.rav@gmail.com>
2020-05-26 10:33:21 +0300

    card pay

--- Client/src/modules/components/pay/CreditCard.tsx
+++ Client/src/modules/components/pay/CreditCard.tsx
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {FormInput} from "../../shared/utils/FormInput"
 import {useForm} from "../../hooks/form.hook"
+import Cards from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
 
 interface Props {
     changeFormHandler: (form: any) => void
@@ -22,31 +24,59 @@ export const CreditCard = (props: Props) => {
 
     const {form, changeHandler} = useForm(initialValues)
 
+    const handleInputFocus = (e : any) => {
+        console.log(typeof e.target.name)
+        if(e.target.name === "cardHolder"){
+            setFocud('name')
+        }
+        if(e.target.name === "month"){
+            setFocud('expiry')
+        }
+        if(e.target.name === "year"){
+            setFocud('expiry')
+        }
+        if(e.target.name === "cardNumber"){
+            setFocud('number')
+        }
+        if(e.target.name === "cVC"){
+            setFocud('cvc')
+        }
+        console.log(e.target.name)
+    }
+
     useEffect(() => {
-        changeFormHandler(form)
+        //changeFormHandler(form)
         // eslint-disable-next-line
     }, [isSubmitting])
 
+    const [focus, setFocud] = useState<"name" | "number" | "expiry" | "cvc">('name')
 
     return (
         <div className="card-details">
             <h3 className="title">Credit Card Details</h3>
+            <FormInput type="hidden" onChange={changeHandler} name={'product'} formValue={form.product}/>
+            <Cards
+                cvc={form.cVC}
+                expiry={form.year+form.month}
+                focused={focus}
+                name={form.cardHolder}
+                number={form.cardNumber}
+            />
             <div className="form-row">
-                <FormInput type="hidden" onChange={changeHandler} name={'product'} formValue={form.product}/>
                 <div className="col-sm-8">
-                    <FormInput onChange={changeHandler} name={'cardNumber'} formValue={form.cardNumber}/>
+                    <FormInput onFocus={handleInputFocus} onChange={changeHandler} name={'cardNumber'} formValue={form.cardNumber}/>
                 </div>
                 <div className="col-sm-4">
-                    <FormInput onChange={changeHandler} name={"cVC"} formValue={form.cVC}/>
+                    <FormInput onFocus={handleInputFocus} onChange={changeHandler} name={"cVC"} formValue={form.cVC}/>
                 </div>
                 <div className="col-sm-6">
-                    <FormInput onChange={changeHandler} name={"month"} formValue={form.month}/>
+                    <FormInput onFocus={handleInputFocus} onChange={changeHandler} name={"month"} formValue={form.month}/>
                 </div>
                 <div className="col-sm-6">
-                    <FormInput onChange={changeHandler} name={"year"} formValue={form.year}/>
+                    <FormInput onFocus={handleInputFocus} onChange={changeHandler} name={"year"} formValue={form.year}/>
                 </div>
                 <div className="col-sm-7">
-                    <FormInput onChange={changeHandler} name={"cardHolder"} formValue={form.cardHolder}/>
+                    <FormInput onFocus={handleInputFocus} onChange={changeHandler} name={"cardHolder"} formValue={form.cardHolder}/>
                 </div>
                 <div className="col-sm-12">
                     <div className="form-group">
