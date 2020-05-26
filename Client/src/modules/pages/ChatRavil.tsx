639ca70c57248ffb771c7a871ebb8806b98aacdd
AlexandrJunushaliev(Hikirangi) <dzhunall@mail.ru>
2020-05-27 03:39:15 +0500

    [add]signalr

--- Client/src/modules/pages/ChatRavil.tsx
+++ Client/src/modules/pages/ChatRavil.tsx
@@ -2,20 +2,28 @@ import React, {useEffect, useState} from 'react'
 import {HubConnection, HubConnectionBuilder} from "@aspnet/signalr"
 import {URL} from "../shared/request"
 import {FormInput} from "../shared/utils/FormInput"
+import {useAuth} from "../hooks/auth.hook";
+import jwt from "jsonwebtoken";
+import {Token} from "../shared/interface";
+import {useHttp} from "../hooks/http.hook";
 
 export const ChatRavil = () => {
-
-    const username = 'Ravil'
+    const a = useAuth().token
+    const decoded = jwt.decode(a! as string)
+    const tokenItems = decoded as Token
+    const [username, setUsername] = useState("")
     const [hubConnection, setHubConnection] = useState<HubConnection>()
     const [something, setSomething] = useState('')
-    const [messages, setMessages] = useState<string[]>(['dasdas', 'dasdas'])
+    const [messages, setMessages] = useState<string[]>([])
+
     const submit = async (event: any) => {
         event.preventDefault()
         setSomething('')
-        await hubConnection!.send('newMessage', username, something)
+        await hubConnection!.send('newMessage', tokenItems.name, something)
     }
 
     useEffect(() => {
+        setUsername(tokenItems?.name ?? "")
         const createHubConnection = async () => {
             const hubConnect = new HubConnectionBuilder()
                 .withUrl(URL + 'api/chat')
@@ -27,6 +35,7 @@ export const ChatRavil = () => {
                     (username: string, message: string) => {
                         setMessages(x => [...x, message])
                     })
+                await hubConnect.send('getAll')
             } catch (err) {
                 console.log(err)
             }
