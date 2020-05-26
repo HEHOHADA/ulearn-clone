e7ffb4836db1d7912c522856a9cca219da6f47bc
AlexandrJunushaliev(Hikirangi) <dzhunall@mail.ru>
2020-05-27 03:54:00 +0500

    [add]fromToSubs

--- Server/UlearnAPI/UlearnData/Models/Subscription.cs
+++ Server/UlearnAPI/UlearnData/Models/Subscription.cs
@@ -1,9 +1,11 @@
-﻿namespace UlearnData.Models
+﻿using System;
+
+namespace UlearnData.Models
 {
     public class Subscription
     {
         public int Id { get; set; }
-        
+        public DateTime BoughtDate { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int Level { get; set; }
