d377b262c6d01cf031c4e1fb05ba12bf74657000
Ravil <graf.rav@gmail.com>
2020-05-26 12:57:08 +0300

    code task video task confirm get

---
+++ Server/UlearnAPI/UlearnData/Models/Tasks/CodeTasks/CodeTask.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UlearnData.Models.Tasks.CodeTasks
+{
+    public class CodeTask
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string InitialCode { get; set; }
+        public int Points { get; set; }
+
+        [JsonIgnore] public Module Module { get; set; }
+    }
+}
